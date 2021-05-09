f4bfeb9710d46addae9ad794ac89e9f7a76c28bd
Exhale Yoga <exhaleyoga@exhales-air.lan>
2021-05-09 16:24:20 -0400

    styling header

--- src/Home/Header.tsx
+++ src/Home/Header.tsx
@@ -5,23 +5,15 @@ import logo from '../Images/logo.png';
 
 const Header = () => {
   return (
-    <div className="container">
-      <Jumbotron
-        className="jumbotron"
-        style={{ backgroundImage: `url(${bgimage})`, backgroundSize: "cover" }}
-      ></Jumbotron>
-      <div className="bar"></div>
-      {/* <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-      </div> */}
-      <hr />
-      {/* <h4 className="text-center">Welcome to Indy Art Store</h4> */}
-    </div>
+    <header id = 'header-background'>
+      <div className="bgimg">
+      </div>
+    </header>
+    // <div className="container">
+    //   <Jumbotron
+    //     className="jumbotron"
+    //     style={{ backgroundImage: `url(${bgimage})`, backgroundSize: "cover" }}
+    //   ></Jumbotron>
   );
 };
 
